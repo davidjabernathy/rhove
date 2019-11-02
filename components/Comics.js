18b20080faf45959049cad992de20c8d853f572b
David Abernathy <djabernathy@att.net>
2019-11-02 14:40:06 -0400

    added the ability to reverse the order of commics. the default is most recent to oldest.

--- components/Comics.js
+++ components/Comics.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, ActivityIndicator, TouchableOpacity, Text } from 'react-native';
+import { View, FlatList, ActivityIndicator, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 import ComicCard from './ComicCard';
 
@@ -9,6 +9,12 @@ export default function Comics(props) {
     const [ mostRecentComicId, setMostRecentComicId ] = useState(null)
     const [ mostRecentComic, setMostRecentComic ] = useState(null)
     const [ lastFetchedComicId, setLastFetchedComicId ] = useState(null)
+    const [ isDescending, setIsDescending ] = useState(true)
+    const [ didMount, setDidMount ] = useState(false)
+     
+    useEffect(() => {
+        setDidMount(true)
+    },[])
 
     useEffect(() => {
         getCurrentComic()
@@ -19,6 +25,24 @@ export default function Comics(props) {
             getPrevFourComics(mostRecentComicId)
         } 
     }, [mostRecentComicId])
+
+    useEffect(() => {
+        if(didMount) {
+            setComicsList([])
+        }
+
+    }, [isDescending])
+
+    useEffect(() => {
+        debugger
+        if(didMount) {
+            if(!isDescending && comicsList.length == 0) {
+                getNextFourComics(0, true)
+            } if(isDescending && comicsList.length == 0) {
+                getPrevFourComics(mostRecentComicId, true)
+            }
+        }
+    }, [comicsList])
       
     getCurrentComic = async function() {   
         let url = 'https://any-api.com:8443/http://xkcd.com/info.0.json'
@@ -32,7 +56,7 @@ export default function Comics(props) {
             setLastFetchedComicId(comic.num)
             
         } catch(ex) {
-            console.log(ex)
+            console.error(ex)
         }
     }
 
@@ -60,13 +84,32 @@ export default function Comics(props) {
         setComicsList(prevComics.concat([first, second, third, fourth]))
     }
 
+    getNextFourComics = async (prevId) => {
+
+        var first = await getComicById(prevId + 1)
+        var second = await getComicById(prevId + 2)
+        var third = await getComicById(prevId + 3)
+        var fourth = await getComicById(prevId + 4)
+
+        const prevComics = comicsList
+        setComicsList(prevComics.concat([first, second, third, fourth]))
+    }
+
     addComic = async function(newComic) {
         const prevComics = comicsList
         setComicsList(prevComics.concat([newComic]))
     }
+
+    reverseOrder = async () => {
+        setIsDescending(!isDescending)  
+    }
     
     if(!comicsList || comicsList.length < 2) {
-        return <ActivityIndicator />
+        return (
+            <View style={styles.centerContainer}>
+                <ActivityIndicator />
+            </View>
+        )
     }
  
     return (
@@ -76,7 +119,19 @@ export default function Comics(props) {
                 <ComicCard comic={item} />
             )}
             keyExtractor={(item) => item.key}
+            ListHeaderComponent={() => {
+                return <TouchableOpacity onPress={() => reverseOrder()}><Text>reverse order</Text></TouchableOpacity>
+            }}
         />
     )
 
 }
+
+styles = StyleSheet.create({
+    centerContainer: {
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
